42d14b177b2bb68a9ca0e355a4b4ee1a441936ad
mingming fan <mingming.fan@mail.utoronto.ca>
2021-07-24 13:20:23 +0800

    add two best paper lists: HCI and Visualization

--- CSV2HTML.py
+++ CSV2HTML.py
@@ -1,27 +1,166 @@
 import csv
 
+
 #CHI
-with open('CHI.csv', 'r',) as input, open('output.txt', 'w', newline='') as output:
+with open('CHI.csv', 'r',) as input, open('CHI.txt', 'w', newline='') as output:
     reader = csv.reader(input, delimiter = ',')
 
     output.write("<table><thead><td colspan=3><a name=\"CHI\">CHI</a></td></thead>");
+    counter = 1
     for row in reader:
         #print(row)
-        output.write("<tr>")
-        output.write("<td rowspan=1>"+ row[0]+ "</td>" + "<td><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td>" + row[2] + "</td>")
-        output.write("</tr>")
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+
+#VIS
+with open('VIS.csv', 'r',) as input, open('VIS.txt', 'w', newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"VIS\">VIS (InfoVis, VAST, SciVis)</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
     output.write("</table>");
     output.write("<br>")
 
 #UbiComp
-with open('UbiComp.csv', 'r',) as input, open('output.txt', 'a', newline='') as output:
+with open('UbiComp.csv', 'r',) as input, open('UbiComp.txt', 'w', newline='') as output:
     reader = csv.reader(input, delimiter = ',')
 
     output.write("<table><thead><td colspan=3><a name=\"UbiComp\">UbiComp/Pervasive/IMWUT</a></td></thead>");
+    counter = 1
     for row in reader:
         #print(row)
-        output.write("<tr>")
-        output.write("<td rowspan=1>"+ row[0]+ "</td>" + "<td><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td>" + row[2] + "</td>")
-        output.write("</tr>")
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+
+#CSCW
+with open('CSCW.csv', 'r',) as input, open('CSCW.txt', 'w',newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"CSCW\">CSCW/PACMHCI</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+
+#UIST
+with open('UIST.csv', 'r',) as input, open('UIST.txt', 'w', newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"UIST\">UIST</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+#IUI
+with open('IUI.csv', 'r',) as input, open('IUI.txt', 'w', newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"IUI\">IUI</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+#DIS
+with open('DIS.csv', 'r',) as input, open('DIS.txt', 'w', newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"DIS\">DIS</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
+    output.write("</table>");
+    output.write("<br>")
+
+
+#ASSETS
+with open('ASSETS.csv', 'r',) as input, open('ASSETS.txt', 'w', newline='') as output:
+    reader = csv.reader(input, delimiter = ',')
+
+    output.write("<table><thead><td colspan=3><a name=\"ASSETS\">ASSETS</a></td></thead>");
+    counter = 1
+    for row in reader:
+        #print(row)
+        if counter == 1:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%>" + row[1] + "</td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        else:
+            output.write("<tr>")
+            output.write("<td width=2.5%>"+ row[0]+ "</td>" + "<td width=37.5%><a href=\"" + row[3] + "\">" + row[1] + "</a></td>" + "<td width=60%>" + row[2] + "</td>")
+            output.write("</tr>")
+        counter = counter + 1
     output.write("</table>");
     output.write("<br>")
